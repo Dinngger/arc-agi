051644c089e4972be8ee67d72fb5493545743b4c
Dinger <dexd@foxmail.com>
2024-08-07 23:50:30 +0800

    very good ladderpath of string

--- ladderpath_str.py
+++ ladderpath_str.py
@@ -1,73 +1,45 @@
+# Author: Dinger
+# Data: 2024-08-07
+# Find the minimum ladderpath-index of a string.
+
 from collections import Counter
+import random
 
-class Component:
-    pass
-
-base_components = {'a', 'b', 'c', 'd'}
-
-class Operator:
-    pass
-
-class Concat(Operator):
-    def __init__(self, xs):
-        self.xs = xs
-    def __len__(self):
-        return len(self.xs)
-
-class Ladderpath:
-    def __init__(self, components):
-        self.components = components
-    def __len__(self):
-        l = 1
-        for s in self.components:
-            for c in s:
-                l += len(c) - 1
-        return l
-
-s = 'abcabdabc'
-# { a, b, c, d // ab // abc // abcabdabc}
-ab = Concat(['a', 'b'])
-abc = Concat([ab, 'c'])
-abcabdabc = Concat([abc, ab, 'd', abc])
-ladderpath = Ladderpath([['a', 'b', 'c', 'd'], [ab], [abc], [abcabdabc]])
-# ( Ladderpath-index:6,  Order-index:3,  Size-index:9 )
-assert len(ladderpath) == 6
-
-
-min_ladderpath = None
-import os
-def find_ladderpath(s: str, cnt=0, his=[]):
-    global min_ladderpath
-    # assert chr(cnt) not in s
-    assert cnt < 10
-    all_2_grams = [s[i:i+2] for i in range(len(s)-1)]
-    count = Counter(all_2_grams)
-    count = [k for k,v in count.items() if v > 1]
-    if not count:
-        if min_ladderpath > len(s) + cnt:
-            print(f'find {len(s) + cnt} length ladderpath')
-            print(f'cnt = {cnt}, s = {s}, his = {his}')
-            if len(s) + cnt == 24:
-                os._exit(0)
-        min_ladderpath = min(min_ladderpath, len(s) + cnt)
-        # print(f'find {len(s) + cnt} length ladderpath {s}')
-        # if min_ladderpath is None:
-        #     min_ladderpath = len(s) + cnt
-        # else:
-        #     assert min_ladderpath <= len(s) + cnt
-    for k in count:
-        # new_s = s.replace(k, str(chr(cnt)))
-        new_s = s.replace(k, str(cnt))
-        # print(f"replacing {k} with {cnt} in {s} gives {new_s}")
-        find_ladderpath(new_s, cnt+1, his+[k])
+def find_ladderpath(s: str):
+    ss = [s]
+    cnt = 0
+    min_lp = len(s)
+    while ss:
+        assert cnt < 65, 'need a new coding method'
+        new_ss = []
+        for s in ss:
+            min_lp = min(min_lp, cnt + len(s))
+            all_2_grams = [s[i:i+2] for i in range(len(s)-1)]
+            count = Counter(all_2_grams)
+            for k,v in count.items():
+                if v > 1:
+                    new_s = s.replace(k, str(chr(cnt)))  # only A-z are allowed in s
+                    new_ss.append(new_s)
+        if new_ss:
+            ss = new_ss
+        lens = [len(s) for s in ss]
+        min_len = min(lens)
+        sampled_ss = []
+        for margin in range(5):
+            m_ss = [s for s in ss if len(s) == min_len + margin]
+            if len(m_ss) > 50:
+                m_ss = random.sample(m_ss, 50)
+            sampled_ss.extend(m_ss)
+        ss = sampled_ss
+        if not new_ss:
+            min_lp = min(min_lp, cnt + min_len)
+            return min_lp
+        cnt += 1
 
 from sys import argv
-min_ladderpath = len(argv[1])
-find_ladderpath(argv[1])
-print(min_ladderpath)
-
-# acsccsaascascaaaacssscscscasasacssacssaaa
-# 0:ac 1:0s 2:aa 3:as 4:sc 5:43 6:2a 7:1s
-# 1ccs25c674453776
-# { a, c, s // 0, 2, 3, 4 // 1, 5, 6 // 7}
-# { a, c, s // ac, aa, as, sc // acs, scas, aaa // acss}
+if len(argv) < 2:
+    s = 'acsccsaascascaaaacssscscscasasacssacssaaaacsccsaascascaaaacssscscscasasacssacssaaa'
+    print(f'using default string: {s}')
+else:
+    s = argv[1]
+print('ladderpath-index =', find_ladderpath(s))
